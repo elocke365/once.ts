be19a4a9eb9f669a73becf7dcce820675ddc62a7
Marcel Donges <marcel@donges.it>
2021-10-31 14:56:11 +0100

    added Web 4.0 header
    Co-authored-by: phibar <phibar@users.noreply.github.com>

--- Once.class.js
+++ Once.class.js
@@ -1,3 +1,28 @@
+/*
+ * The Web 4.0 ™ platform is supported by enterprise level subscription through Cerulean Circle GmbH
+ *    Copyright (C) 2017  Marcel Donges (marcel@donges.it)
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU Affero General Public License as
+ *    published by the Free Software Foundation, either version 3 of the
+ *    License, or (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Affero General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Affero General Public License
+ *    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+      {
+            license: "AGPL3.0",
+            href: "http://www.gnu.org/licenses/agpl-3.0.de.html"
+            coAuthors: [
+                "Philipp Bartels",
+                }
+      }
+ */
 class Once {
     static async start() {
         let ONCE = new Once();
